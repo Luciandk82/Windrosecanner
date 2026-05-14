ead8d969b447cdfe0eabf9ffec9c04674454c425
Jimmy Larsen <larsen.jimmy@gmail.com>
2026-05-15 01:11:04 +0200

    Add UE4SS lifecycle exports to RTNativeExporter

--- cpp-mods/RTNativeExporter/dllmain.cpp
+++ cpp-mods/RTNativeExporter/dllmain.cpp
@@ -1,46 +1,88 @@
-#include <Windows.h>
+#define NOMINMAX
+
+#include <Mod/CppUserModBase.hpp>
+#include <DynamicOutput/DynamicOutput.hpp>
+#include <windows.h>
 #include <fstream>
 #include <filesystem>
-#include <string>
 #include <chrono>
 #include <thread>
 
-static std::filesystem::path OutDir()
+using namespace RC;
+
+static std::filesystem::path resolve_data_dir()
 {
-    return std::filesystem::current_path() / "windrose_plus_data" / "native_rt_export";
+    std::filesystem::path candidates[] = {
+        "../../../windrose_plus_data",
+        "windrose_plus_data",
+    };
+
+    for (auto& p : candidates)
+    {
+        try
+        {
+            if (std::filesystem::exists(p)) return p;
+        }
+        catch (...) {}
+    }
+
+    try { std::filesystem::create_directories("windrose_plus_data"); } catch (...) {}
+    return "windrose_plus_data";
 }
 
-static void Log(const std::string& s)
-{
-    std::filesystem::create_directories(OutDir());
-    std::ofstream f(OutDir() / "rt_native_exporter.log", std::ios::app);
-    f << s << "\n";
-}
-
-DWORD WINAPI MainThread(LPVOID)
+static void file_log(const std::string& s)
 {
     try
     {
-        std::this_thread::sleep_for(std::chrono::seconds(30));
-        Log("RTNativeExporter loaded");
-        Log("Current path=" + std::filesystem::current_path().string());
-        Log("Phase 1 skeleton OK");
+        auto out_dir = resolve_data_dir() / "native_rt_export";
+        std::filesystem::create_directories(out_dir);
+        std::ofstream f(out_dir / "rt_native_exporter.log", std::ios::app);
+        f << s << "\n";
     }
-    catch (...)
-    {
-        Log("RTNativeExporter exception");
-    }
-
-    return 0;
+    catch (...) {}
 }
 
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID)
+class RTNativeExporter : public CppUserModBase
 {
-    if (reason == DLL_PROCESS_ATTACH)
+public:
+    RTNativeExporter() : CppUserModBase()
     {
-        DisableThreadLibraryCalls(hModule);
-        CreateThread(nullptr, 0, MainThread, nullptr, 0, nullptr);
+        ModName = STR("RTNativeExporter");
+        ModVersion = STR("0.1.0");
     }
 
-    return TRUE;
+    ~RTNativeExporter() override {}
+
+    auto on_unreal_init() -> void override
+    {
+        Output::send<LogLevel::Verbose>(STR("[RTN] RTNativeExporter on_unreal_init\n"));
+        file_log("RTNativeExporter on_unreal_init");
+        file_log("Phase 1 lifecycle OK");
+    }
+
+    auto on_update() -> void override
+    {
+        m_frame_count++;
+
+        if (!m_logged_update && m_frame_count > 60)
+        {
+            m_logged_update = true;
+            Output::send<LogLevel::Verbose>(STR("[RTN] RTNativeExporter on_update OK\n"));
+            file_log("RTNativeExporter on_update OK");
+        }
+    }
+
+private:
+    int m_frame_count = 0;
+    bool m_logged_update = false;
+};
+
+extern "C" __declspec(dllexport) RC::CppUserModBase* start_mod()
+{
+    return new RTNativeExporter();
+}
+
+extern "C" __declspec(dllexport) void uninstall_mod(RC::CppUserModBase* mod)
+{
+    delete mod;
 }
